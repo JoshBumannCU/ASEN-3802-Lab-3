4748598b9c2fc9a54ba4659c6bd00af6b5b51f34
Colton-Firster-CU <cofi4156@colorado.edu>
2026-03-31 12:56:40 -0600

    Added Equiangular X function
    takes input c and outputs x

---
+++ Equiangular_Rays.m
@@ -0,0 +1,19 @@
+function [x] = Equiangular_Rays(c)
+% Summary: creates x-coordinate vector through equiangular lines
+%   Creates x-coordinate vector using equiangular rays of radius half-chord
+%   about a center at half chord. This creates a x vector that is denser
+%   around areas where the airfoil would have more curvature (i.e. the
+%   leading edge and trialing edge), starting at the leading edge and
+%   ending and the trailing edge.
+%   Inputs: 
+%   c: chord length (m)
+%   Outputs:
+%   x: x-coordinate vector created via equiangular rays (m)
+%   
+%   Author: Colton Firster
+%   Collaborators: Josh Bumann, Clara Eide, Lane Hollis
+%   Date: 3/31/2026
+
+theta = 0:1:180; % angle vector from 0 to 180 deg equally spaced by 1 deg
+x = c/2 - c/2 * cosd(theta); % x vector from equiangular rays
+end
