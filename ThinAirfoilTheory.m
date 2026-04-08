ccc15f5db1ffea9c801cfb1045a357841917dcb6
claraeide <clei4392@colorado.edu>
2026-04-08 16:19:00 -0600

    Add files via upload
    edited

--- ThinAirfoilTheory.m
+++ ThinAirfoilTheory.m
@@ -4,15 +4,18 @@
 %   m: max camber
 %   c: chord length (m)
 %   p: postition of max camber
-%   AoA: Angle of attack (alpha)
+%   AoA: Angle of attack (alpha) - NEED INPUT IN DEG
 %   Outputs: 
-%   cl: lift coeffiecent from thin airfoil theory
+%   cl_ThinAirfoilTheory: lift coeffiecent from thin airfoil theory
+%   ZeroLiftAoA: Zero lift angle of attack for airfoil
 %   Author: Clara Eide
 %   Collaborators: Josh Bumann, Colton Firster, Lane Hollis
 %   Date: 4/7/2026
 
-function cl_ThinAirfoilTheory = ThinAirfoilTheory(c, m, p, AoA)
+function [cl_ThinAirfoilTheory,ZeroLiftAoA] = ThinAirfoilTheory(c, m, p, AoA)
 %calculating cl with Thin Airfoil Theory
+
+AoA = deg2rad(AoA); % convert AoA to rad!!
 
 fun = @(theta) dydx(theta, c, m, p) .* (cos(theta) - 1); % creates the function we need to integrate
 
@@ -34,7 +37,7 @@ for i = 1:length(theta) % creates piecewise from doc
     if theta(i) < bound
         dydx(i) = ( (2*m / p) * (1 - (x(i)./(p*c)) ) );
     else
-        dydx(i) = ( 2*m/(1-p)^2 * (p - x(i) / c) );
+        dydx(i) = ( (2*m/(1-p)^2) * (p - x(i) / c) );
     end
 end 
 
